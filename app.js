9f7e4bac9cb744cbc587a560a07d72682c71de5c
Akobir5 <toshtemirovakobir8.com>
2022-04-25 15:29:53 +0500

    first clone

---
+++ app.js
@@ -0,0 +1,54 @@
+let wrepper = document.querySelector('.wrepper')
+let navigation = document.querySelector('.navigation')
+let Apple = document.querySelector('moviesDB')
+
+
+
+
+const moviesDB = [
+    {
+        pathImg: "https://ds-assets.cdn.devapps.ru/LaKbqW14FImz1OPYpbe70vEZCENQY.png",
+        nemeMovies: "8 причин перейти на OLED",
+        lorem: 'От железа все ждут максимальную производительность при минимальных энергопотреблении и тепловыделении. А от экранов — топовую картинку и высочайшую яркость при небольшой толщине и скромном потреблении электричества. К тому же на лэптопах куда чаще встречаются статичные изображения, которые так не любят OLED-панели. Несмотря на всё это, именно экраны на органических светодиодах — лучший выбор для ноутбуков, и вот почему.',
+        data: "24.01.2022",
+        img: 'https://tech-all-news.netlify.app/icons/message.png '
+    },
+    {
+        pathImg: "https://ds-assets.cdn.devapps.ru/LaKbqW14FImz1OPYpbe70vEZCENQY.png",
+        nemeMovies: "8 причин перейти на OLED",
+        lorem: 'От железа все ждут максимальную производительность при минимальных энергопотреблении и тепловыделении. А от экранов — топовую картинку и высочайшую яркость при небольшой толщине и скромном потреблении электричества. К тому же на лэптопах куда чаще встречаются статичные изображения, которые так не любят OLED-панели. Несмотря на всё это, именно экраны на органических светодиодах — лучший выбор для ноутбуков, и вот почему.',
+        data: "24.01.2022",
+        img: 'https://tech-all-news.netlify.app/icons/message.png '
+    },
+    {
+        pathImg: "https://ds-assets.cdn.devapps.ru/LaKbqW14FImz1OPYpbe70vEZCENQY.png",
+        nemeMovies: "8 причин перейти на OLED",
+        lorem: 'От железа все ждут максимальную производительность при минимальных энергопотреблении и тепловыделении. А от экранов — топовую картинку и высочайшую яркость при небольшой толщине и скромном потреблении электричества. К тому же на лэптопах куда чаще встречаются статичные изображения, которые так не любят OLED-панели. Несмотря на всё это, именно экраны на органических светодиодах — лучший выбор для ноутбуков, и вот почему.',
+        data: "24.01.2022",
+        img: 'https://tech-all-news.netlify.app/icons/message.png '
+    }
+
+]
+
+
+moviesDB.forEach(item => {
+    let wrepper = document.createElement('div')
+        wrepper.classList.add('wrepper  ')
+    let navigation = document.createElement('div')
+        navigation.classList.add(navigation)
+    let img = document.createElement('img')
+    let neme = document.createElement
+    let reponce = document.createElement('a')
+    let buttun_tag = document.createElement('button')
+    let buttun = document.createElement('img')
+    let card = document.createElement('lorem')
+
+
+    strong.innerText = item.riting
+    data.innerText = item.data
+
+noviesDB.append('card');
+card.append(h3);
+lorem.append(p); 
+})
+
